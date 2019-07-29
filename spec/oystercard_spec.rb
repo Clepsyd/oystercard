ea5b0b73dc4f6ab6630c96ca83f513bc06c8044b
Guillaume <liard.guillaume@gmail.com>
2019-07-29 16:09:13 +0100

    Added: DomainModel + test for #top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
   it { is_expected.to respond_to :balance }
+
+  describe "#top_up" do
+    it "raises the balance by a certain amount" do
+      balance = subject.balance
+      subject.top_up(10)
+      expect(subject.balance).to eq balance + 10
+    end
+  end
 end
