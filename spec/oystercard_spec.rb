9b8d91c2076462f628416f23487b89df5d4d35b7
Jodi <jodiallysonfoster@gmail.com>
2019-07-29 15:33:36 +0100

    Maximum balance added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,5 +9,10 @@ describe Oystercard do
       subject.top_up(10)
       expect(subject.balance).to eq balance + 10
     end
+
+    it "raises an error when limit is exceeded" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      expect{ subject.top_up(maximum_balance + 1) }.to raise_error "Limit is £90"
   end
+end
 end
